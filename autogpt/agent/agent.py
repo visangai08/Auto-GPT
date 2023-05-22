c68437bf9262de4c5050d4e890c9236c9509ed2a
visangai08 <visangai08@gmail.com>
2023-05-22 05:22:18 +0000

    "thinking..."

--- autogpt/agent/agent.py
+++ autogpt/agent/agent.py
@@ -125,7 +125,7 @@ class Agent:
                 )
                 break
             # Send message to AI, get response
-            with Spinner("생각... "):
+            with Spinner("생각중... "):
                 assistant_reply = chat_with_ai(
                     self,
                     self.system_prompt,
